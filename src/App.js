939b3a942818dfe9c78a7939f3a499655abf15cc
JAMES <jameskibunja17@gmail.com>
2023-02-23 15:01:29 +0300

    Added weather component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
-import React from 'react';
-import Navigation from './Navigation';
-import Weather from './Weather';
-import ActivityList from './ActivityList';
-import SelectedActivities from './SelectedActivities';
-import Share from './Share';
+import './App.css';
+import Component2 from './Component2';
+import Component3 from './Component3';
+import Component4 from './Component4';
+import Navigation from './Navigation'
+import Weather from './Weather'
 
-function WeatherApp() {
-  const activities = [
-    { id: 1, name: 'Go for a hike', weatherType: 'Clear' },
-    { id: 2, name: 'Go to the beach', weatherType: 'Clear' },
-    { id: 3, name: 'Go skiing', weatherType: 'Snow' },
-    { id: 4, name: 'Go ice skating', weatherType: 'Snow' },
-    { id: 5, name: 'Go to the movies', weatherType: 'Rain' },
-    { id: 6, name: 'Play board games', weatherType: 'Rain' },
-  ];
-
+function App() {
   return (
-    <div>
-      <Navigation />
+    <div className="App">
+      <div className="Navigation">
+        <Navigation />
+      </div>
+      <div className='grid'>
       <Weather />
-      <ActivityList weatherType="Clear" activities={activities} />
-      <ActivityList weatherType="Snow" activities={activities} />
-      <ActivityList weatherType="Rain" activities={activities} />
-      <SelectedActivities />
-      <Share />
+      <Component2 />
+      <Component3 />
+      <Component4 />
+
+      </div>
+      
     </div>
   );
 }
 
-export default WeatherApp;
+export default App;
