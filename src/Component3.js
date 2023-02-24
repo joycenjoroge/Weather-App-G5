de3faad54b54d6754a89dba5a3fe874393a87180
Joyce <joycenjoroge47@gmail.com>
2023-02-24 12:18:13 +0300

    Added the activtiy list component

--- src/Component3.js
+++ src/Component3.js
@@ -1,10 +1,43 @@
 import React from "react";
+import { useState } from "react";
 
 function Component3 (){
 
+ const [activities, setActivityList] = useState([
+  { id: 1, name: 'Go for a hike', weatherType: 'Clear' },
+  { id: 2, name: 'Go to the beach', weatherType: 'Clear' },
+  { id: 3, name: 'Go skiing', weatherType: 'Snow' },
+  { id: 4, name: 'Go ice skating', weatherType: 'Snow' },
+  { id: 5, name: 'Go to the movies', weatherType: 'Rain' },
+  { id: 6, name: 'Play board games', weatherType: 'Rain' },
+]);
+
+
+  //  useEffect(() => {
+  //   setActivityList(activities);
+  // }, []);
+  
+
+  const deleteById = id => {
+    setActivityList(oldValues => {
+      return oldValues.filter(activity => activity.id !== id)
+    })
+  }
     return(
         <>
-        <h2>Component3</h2>
+        <div>
+        <h2>Your Saved Activites</h2>
+        <ul className="activites">
+        {activities.map((activity)=>(
+        <li>
+          <span>{activity.name}</span>
+          <button onClick={() => deleteById(activity.id)} className="remove">Remove</button>
+          </li>
+          ))}
+        </ul>
+        </div>
+       
+        
         </>
     )
 }
